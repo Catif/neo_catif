8d9943fbefed5e2b7309ecff4986e01e5d6e7634
Bradley BARBIER <bradley.barbier@outlook.fr>
2024-06-16 18:29:41 +0200

    feat: create a palette colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,21 @@ export default {
   ],
   theme: {
     fontFamily: {
-      sans: ['Source Sans Pro', 'sans-serif'],
-      serif: ['Source Sans Pro', 'serif'],
+      sans: ['Daydream', 'sans-serif'],
+      serif: ['Daydream', 'serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      
+      'background': '#351F39',
+      'card': "#726A95",
+      
+      'primary': "#719FB0",
+      'secondary': "#A0C1B8",
+
+      'text': "#DEE9ED",
+      'paragraph': "#A6A5B1",
+      'link': "#719FB0"
     },
   },
   plugins: [],
