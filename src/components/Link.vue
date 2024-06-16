87cc676ae2c41e134ec80250452e5de22ec10b58
Bradley BARBIER <bradley.barbier@outlook.fr>
2024-06-16 18:32:33 +0200

    feat: create Link component

---
+++ src/components/Link.vue
@@ -0,0 +1,32 @@
+<script setup>
+import TextWave from './TextWave.vue';
+
+const props = defineProps({
+  to: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  }
+})
+
+
+</script>
+
+
+
+
+
+<template>
+  <RouterLink :to="to" class="group">
+    <TextWave :text="text" :alreadyGrouped="true" />
+  </RouterLink>
+</template>
+
+
+
+
+
+<style scoped></style>
