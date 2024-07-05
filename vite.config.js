1978c85c03735f47278a66835a0a30811059b36b
Catif <bradley.barbier@outlook.fr>
2024-07-05 22:41:38 +0200

    feat(package): add auto-imports and sass

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,34 @@
-import { URL, fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { URL, fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: [
+        {
+          vue: [
+            "ref",
+            "reactive",
+            "computed",
+            "watch",
+            "onMounted",
+            "onUnmounted",
+            "provide",
+            "inject",
+          ],
+        },
+        {
+          "vue-router": ["useRoute", "useRouter"],
+        },
+      ],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
