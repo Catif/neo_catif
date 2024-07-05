c9cd2d47f9fc33c79c91e2453e4d76abc4d4d6bc
Catif <bradley.barbier@outlook.fr>
2024-07-05 22:43:01 +0200

    feat(TextWave): remove useless props

--- src/components/TextWave.vue
+++ src/components/TextWave.vue
@@ -2,46 +2,22 @@
 const props = defineProps({
   text: {
     type: String,
-    required: true
+    required: true,
   },
-  alreadyGrouped: {
-    type: Boolean,
-    default: false
-  }
-})
-
-
+});
 </script>
 
-
-
-
-
 <template>
-  <span v-if="!alreadyGrouped" class="group">
+  <span class="group/textWave">
     <span
       v-for="(letter, index) in text"
       :key="letter"
-      class="bottom-0 relative group-hover:bottom-1 transition-all duration-300 "
-      :style="'transition-delay: ' + (index * 50) + 'ms'"
+      class="bottom-0 relative group-hover/textWave:bottom-1 transition-all duration-300"
+      :style="'transition-delay: ' + index * 50 + 'ms'"
     >
       {{ letter }}
     </span>
   </span>
-  <template v-else>
-    <span
-      v-for="(letter, index) in text"
-      :key="letter"
-      class="bottom-0 relative group-hover:bottom-1 transition-all duration-300 "
-      :style="'transition-delay: ' + (index * 50) + 'ms'"
-    >
-      {{ letter }}
-    </span>
-  </template>
 </template>
-
-
-
-
 
 <style scoped></style>
