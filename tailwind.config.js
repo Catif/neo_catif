f7d988b73f1f2e658016e4399ee9457ca7e2dbdd
Catif <bradley.barbier@outlook.fr>
2024-07-06 14:58:56 +0200

    style: format prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.vue",
-  ],
+  content: ["./index.html", "./src/**/*.vue"],
   theme: {
     fontFamily: {
-      sans: ['Daydream', 'sans-serif'],
-      serif: ['Daydream', 'serif'],
+      sans: ["Daydream", "sans-serif"],
+      serif: ["Daydream", "serif"],
     },
     colors: {
-      transparent: 'transparent',
-      
-      'background': '#351F39',
-      'card': "#726A95",
-      
-      'primary': "#719FB0",
-      'secondary': "#A0C1B8",
+      transparent: "transparent",
 
-      'text': "#DEE9ED",
-      'paragraph': "#A6A5B1",
-      'link': "#719FB0"
+      background: "#351F39",
+      card: "#726A95",
+
+      primary: "#719FB0",
+      secondary: "#A0C1B8",
+
+      text: "#DEE9ED",
+      paragraph: "#A6A5B1",
+      link: "#719FB0",
     },
   },
   plugins: [],
-}
-
+};
