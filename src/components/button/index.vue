829a2d79248ec88fa17725c40d3a87220c767c1f
Catif <bradley.barbier@outlook.fr>
2024-07-06 16:38:34 +0200

    feat(Components): create Button component

---
+++ src/components/button/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <button
+    class="flex items-center justify-center p-4 bg-card text-white pixel-corners transform transition group/textWave"
+  >
+    <TextWave :text="label" />
+  </button>
+</template>
+
+<style scoped></style>
