64ad6be1d325e4912a4214fa1b9c6b805d980f11
Catif <bradley.barbier@outlook.fr>
2024-07-12 20:48:21 +0200

    feat(app) remove transition between navigation

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
 
-import Navbar from './components/Navbar.vue'
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
-  
+
   <article class="min-h-full w-full">
     <RouterView v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </transition>
+      <component :is="Component" />
     </RouterView>
   </article>
 </template>
 
-
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
-	transition: all 0.2s ease;
+  transition: all 0.2s ease;
 }
 .fade-enter-from,
 .fade-leave-to {
-	opacity: 0;
+  opacity: 0;
 }
 </style>
